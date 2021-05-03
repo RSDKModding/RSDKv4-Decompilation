fac867b39fd650ec921cfa1a2f2f2de381bdc5b0
Rubberduckycooly <rubberduckycooly@outlook.com>
2021-05-03 23:19:09 +1000

    mods will no longer be loaded if they don't have a mod.ini file

--- RSDKv4/Userdata.cpp
+++ RSDKv4/Userdata.cpp
@@ -1005,8 +1005,9 @@ void initMods()
                         modSettings.GetBool("", "TxtScripts", &info->useScripts);
                         if (info->useScripts && info->active)
                             forceUseScripts = true;
+
+                        modCount++;
                     }
-                    modCount++;
                 }
             }
         } catch (std::filesystem::filesystem_error fe) {
