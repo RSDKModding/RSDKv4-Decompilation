5a389ccf81bcf35d7054c45e984cca303fc3cf4d
Leonx254 <59059689+Leonx254@users.noreply.github.com>
2021-09-05 19:24:56 -0300

    Create config_types.h

---
+++ dependencies/android/config_types.h
@@ -0,0 +1,11 @@
+#ifndef __CONFIG_TYPES_H__
+#define __CONFIG_TYPES_H__
+
+typedef short ogg_int16_t;
+typedef unsigned short ogg_uint16_t;
+typedef int ogg_int32_t;
+typedef unsigned int ogg_uint32_t;
+typedef long ogg_int64_t;
+typedef unsigned long ogg_uint64_t;
+
+#endif
