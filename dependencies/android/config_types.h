d1f7a791cf036737c27624ade915b96e59aeb904
Leonx254 <59059689+Leonx254@users.noreply.github.com>
2021-09-05 20:26:24 -0300

    Update config_types.h

--- dependencies/android/config_types.h
+++ dependencies/android/config_types.h
@@ -1,11 +1,12 @@
 #ifndef __CONFIG_TYPES_H__
 #define __CONFIG_TYPES_H__
 
-typedef short ogg_int16_t;
-typedef unsigned short ogg_uint16_t;
-typedef int ogg_int32_t;
-typedef unsigned int ogg_uint32_t;
-typedef long ogg_int64_t;
-typedef unsigned long ogg_uint64_t;
+#include <stdint.h>
+typedef int16_t ogg_int16_t;
+typedef uint16_t ogg_uint16_t;
+typedef int32_t ogg_int32_t;
+typedef uint32_t ogg_uint32_t;
+typedef int64_t ogg_int64_t;
+typedef uint64_t ogg_uint64_t;
 
 #endif
