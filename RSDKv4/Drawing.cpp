63c101f3d4a3a32d3a8868bfea68f59b856a86bc
Rubberduckycooly <rubberduckycooly@outlook.com>
2021-09-12 14:51:50 +1000

    when the vsync hits

--- RSDKv4/Drawing.cpp
+++ RSDKv4/Drawing.cpp
@@ -129,7 +129,7 @@ int InitRenderDevice()
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
     SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
     SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 16);
-    SDL_GL_SetSwapInterval(0);
+    SDL_GL_SetSwapInterval(Engine.vsync ? 1 : 0);
 #endif
 #endif
 #if RETRO_DEVICETYPE == RETRO_MOBILE
