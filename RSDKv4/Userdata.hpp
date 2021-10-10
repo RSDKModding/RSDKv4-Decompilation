47913ea91c08634f241d0fe27374913198e02841
Rubberduckycooly <rubberduckycooly@outlook.com>
2021-10-10 15:03:55 +1100

    save gaming

--- RSDKv4/Userdata.hpp
+++ RSDKv4/Userdata.hpp
@@ -169,7 +169,7 @@ inline bool ReadSaveRAMData()
 #else
         sprintf(buffer, "%sSGame.bin", gamePath);
 #endif
-        saveFile = fOpen(buffer, "wb");
+        saveFile = fOpen(buffer, "rb");
         if (!saveFile)
             return false;
         useSGame = true;
