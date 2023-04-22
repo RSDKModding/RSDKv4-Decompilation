beb8197b4d0668454f9d8d0ee09349c92199a3ff
MegAmi <angrybirdconnor@gmail.com>
2023-04-21 21:26:43 -0500

    when the SS is custom

--- RSDKv4/Userdata.hpp
+++ RSDKv4/Userdata.hpp
@@ -49,6 +49,8 @@ struct SaveGame {
     int unlockedHPZ;     // Value 46
     int unused[17];      // Values 47-63
     int records[0x80];   // Values 64-192
+    int padding[0x73F];  // Values 193-2047
+    int customSS[0x400]; // Values 2048-3072
 };
 
 enum OnlineMenuTypes {
