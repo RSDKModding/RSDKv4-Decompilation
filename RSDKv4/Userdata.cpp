e521ecfe71884fc08c8960279301edd4b63dcebb
MegAmi <angrybirdconnor@gmail.com>
2023-10-29 13:41:13 -0500

    wait, formatting, right [skip ci]

--- RSDKv4/Userdata.cpp
+++ RSDKv4/Userdata.cpp
@@ -1243,9 +1243,7 @@ void NotifyCallback(int *callback, int *param1, int *param2, int *param3)
         case NOTIFY_GOTO_FUTURE_PAST: PrintLog("NOTIFY: GotoFuturePast() -> %d", *param1); break;
         case NOTIFY_BOSS_END: PrintLog("NOTIFY: BossEnd() -> %d", *param1); break;
         case NOTIFY_SPECIAL_END: PrintLog("NOTIFY: SpecialEnd() -> %d", *param1); break;
-        case NOTIFY_DEBUGPRINT:
-            PrintLog("NOTIFY: DebugPrint() -> %d", *param1);
-            break;
+        case NOTIFY_DEBUGPRINT: PrintLog("NOTIFY: DebugPrint() -> %d", *param1); break;
         case NOTIFY_KILL_BOSS: PrintLog("NOTIFY: KillBoss() -> %d", *param1); break;
         case NOTIFY_TOUCH_EMERALD: PrintLog("NOTIFY: TouchEmerald() -> %d", *param1); break;
         case NOTIFY_STATS_ENEMY: PrintLog("NOTIFY: StatsEnemy() -> %d, %d, %d", *param1, *param2, *param3); break;
