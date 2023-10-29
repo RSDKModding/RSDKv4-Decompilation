fc08172ce209db2059cbe4a287dca32e5f8bd701
MegAmi <angrybirdconnor@gmail.com>
2023-10-29 13:54:42 -0500

    comment [skip ci]

--- RSDKv4/Userdata.cpp
+++ RSDKv4/Userdata.cpp
@@ -1243,7 +1243,11 @@ void NotifyCallback(int *callback, int *param1, int *param2, int *param3)
         case NOTIFY_GOTO_FUTURE_PAST: PrintLog("NOTIFY: GotoFuturePast() -> %d", *param1); break;
         case NOTIFY_BOSS_END: PrintLog("NOTIFY: BossEnd() -> %d", *param1); break;
         case NOTIFY_SPECIAL_END: PrintLog("NOTIFY: SpecialEnd() -> %d", *param1); break;
-        case NOTIFY_DEBUGPRINT: PrintLog("NOTIFY: DebugPrint() -> %d", *param1); break;
+        case NOTIFY_DEBUGPRINT:
+            // Although there are instances of this being called from both CallNativeFunction2 and CallNativeFunction4 in Origins' scripts, there's no way we can tell which one was used here to handle possible errors
+            // Due to this, we'll only print param1 regardless of the opcode used
+            PrintLog("NOTIFY: DebugPrint() -> %d", *param1);
+            break;
         case NOTIFY_KILL_BOSS: PrintLog("NOTIFY: KillBoss() -> %d", *param1); break;
         case NOTIFY_TOUCH_EMERALD: PrintLog("NOTIFY: TouchEmerald() -> %d", *param1); break;
         case NOTIFY_STATS_ENEMY: PrintLog("NOTIFY: StatsEnemy() -> %d, %d, %d", *param1, *param2, *param3); break;
