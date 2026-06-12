583377095747b67d5746096a39280d3264b41786
Mefiresu <15063879+Mefiresu@users.noreply.github.com>
2026-06-12 10:07:56 +0200

    Fix gamecontrollerDB crash

--- RSDKv4/Userdata.cpp
+++ RSDKv4/Userdata.cpp
@@ -624,7 +624,7 @@ void InitUserdata()
 
         int nummaps = SDL_GameControllerAddMappingsFromFile(buffer);
         if (nummaps >= 0)
-            PrintLog("loaded %d controller mappings from '%s'\n", buffer, nummaps);
+            PrintLog("loaded %d controller mappings from '%s'", nummaps, buffer);
     }
 #endif
 
